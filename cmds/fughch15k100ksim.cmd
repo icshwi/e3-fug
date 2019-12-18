ce93de1d05219481460fc26214db468988e7671f
antonisimelio <antonisimelio@ci0021274.esss.lu.se>
2019-12-18 09:36:28 +0100

    Before Test in Lab

--- cmds/fughch15k100ksim.cmd
+++ cmds/fughch15k100ksim.cmd
@@ -2,9 +2,10 @@
 #require ps-fug, nsenaud
 
 require stream,2.8.8
-
 require ps_fug, 2.0.0
 
+
+epicsEnvSet("STREAM_PROTOCOL_PATH", "$(ps_fug_DB)")
 
 ## Asyn
 drvAsynIPPortConfigure("HVPS", "127.0.0.1:9999")
--- cmds/fughcp353500sim.cmd
+++ cmds/fughcp353500sim.cmd
@@ -3,6 +3,8 @@ require stream,2.8.8
 require ps_fug,2.0.0
 
 
+epicsEnvSet("STREAM_PROTOCOL_PATH", "$(ps_fug_DB)")
+
 ## Asyn
 drvAsynIPPortConfigure("RepPS-01", "127.0.0.1:9999")
 drvAsynIPPortConfigure("RepPS-02", "127.0.0.1:9999")
